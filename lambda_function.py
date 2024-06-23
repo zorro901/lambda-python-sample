e958ae60fe3be4d4b6975af9d68bfb143809b9f1
zorro901 <zoro907@gmail.com>
2024-06-23 13:45:36 +0900

    migrate fastapi

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,23 @@
-import sys
-def handler(event, context):
-    return 'Hello from AWS Lambda using Python' + sys.version + '!'
+from fastapi import FastAPI
+from pydantic import BaseModel
+from mangum import Mangum
+
+class Item(BaseModel):
+    name: str
+    price: float
+
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {"message": "Hello from lambda"}
+
+@app.get("/uppercase")
+async def uppercase(text: str):
+    return {"message": text.upper()}
+
+@app.post("/items")
+async def create_item(item: Item):
+    return item
+
+handler = Mangum(app)
